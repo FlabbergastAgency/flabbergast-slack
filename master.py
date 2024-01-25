a491e5f99148e89fdbf7ae6e93cde42de66c9cfa
vulama <filip.vulama@gmail.com>
2024-01-25 14:26:33 +0100

    Implement Dynamic Slave Adding

--- master.py
+++ master.py
@@ -2,16 +2,13 @@ import slack_sdk
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from slackeventsapi import SlackEventAdapter
 import webbrowser
 from urllib.parse import urlparse, parse_qs, unquote, urlencode
 import json
 from zoomus import ZoomClient
 import requests
-
-main_meeting_room_names = ['1', 'big', 'main']
-secondary_meeting_room_names = ['2', 'small', 'secondary']
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -22,7 +19,108 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/ev
 slack_client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = slack_client.api_call("auth.test")['user_id']
 
-last_know_url = ""
+last_known_url = ""
+active_slaves = {}
+
+
+def ping_slave(ip):
+    try:
+        response = requests.get(f'http://{ip}:42096/ping', timeout=5)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
+
+
+def ping_all_slaves():
+    inactive_slaves = []
+
+    for id, data in active_slaves.items():
+        ip, name = data
+        response = ping_slave(ip)
+        print(name, ip)
+        if response:
+            print(f"Slave {name} is active.")
+        else:
+            print(f"Slave {name} is not responding. Removing from active list.")
+            inactive_slaves.append(id)
+
+    for slave in inactive_slaves:
+        del active_slaves[slave]
+
+
+def create_button(room, action):
+    return {
+        "type": "button",
+        "text": {
+            "type": "plain_text",
+            "text": room[1][1]
+        },
+        "action_id": action + ":" + room[0]
+    }
+
+
+def generate_blocks(title, channel_id, meeting_rooms, action):
+    main = [
+        {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": "Main Meeting Room"
+            },
+            "action_id": action + ":main"
+        }
+    ]
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": title
+            }
+        },
+        {
+            "type": "actions",
+            "elements":
+                main + [create_button(room, action) for room in meeting_rooms.items()]
+        }
+    ]
+
+    message_payload = {
+        "channel": channel_id,
+        "blocks": blocks
+    }
+
+    return message_payload
+
+
+@app.route('/slaves', methods=['GET'])
+def get_active_slaves():
+    ping_all_slaves()
+    print(
+        generate_blocks(
+            "Select a meeting room where you want to open a link\n*Available Meeting Rooms:*",
+            "#test",
+            active_slaves,
+            "open",
+        )
+    )
+    return jsonify(active_slaves)
+
+
+@app.route('/register', methods=['POST'])
+def register_slave():
+    data = request.json
+    name = data.get('name')
+    ip = data.get('ip')
+    id = data.get('id')
+
+    if name and ip:
+        active_slaves[id] = (ip, name)
+        print(active_slaves)
+        print(f"Registered slave: {name} at {ip}")
+        return "Registration successful", 200
+    else:
+        return "Invalid registration data", 400
 
 
 def extract_zoom_info(zoom_url):
@@ -55,9 +153,9 @@ def build_zoommtg_url(meeting_number, password):
     return zoommtg_url
 
 
-def send_request_to_slave(path, data):
+def send_request_to_slave(path, data, url):
     # Make the POST request
-    response = requests.post('http://192.168.1.83:42096' + path, data=data)
+    response = requests.post('http://' + url + ':42096' + path, data=data)
 
     # Check the status code of the response
     if response.status_code == 200:
@@ -80,46 +178,21 @@ def open_url():
     channel_id = data.get('channel_id')
     url = parsed_data[-1]
 
-    # Parsing of the Zoom link
+
+    ping_all_slaves()
+
     if 'zoom.us' in url:
         meeting_id, password = extract_zoom_info(url)
         url = build_zoommtg_url(meeting_id, password)
 
     last_know_url = url
 
-    message_payload = {
-        "channel": channel_id,
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Select a meeting room where you want to open a link\n*Available Meeting Rooms:*"
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Main Meeting Room"
-                        },
-                        "action_id": "open:main"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Secondary Meeting Room"
-                        },
-                        "action_id": "open:secondary"
-                    }
-                ]
-            }
-        ]
-    }
+    message_payload = generate_blocks(
+        "Select a meeting room where you want to open a link\n*Available Meeting Rooms:*",
+        channel_id,
+        active_slaves,
+        "open",
+    )
 
     slack_client.chat_postMessage(**message_payload)
 
@@ -183,13 +256,15 @@ def slack_interactive():
     if extracted_command[0] == 'create':
         if extracted_command[1] == 'main':
             create_zoom_local(channel_id)
-        elif extracted_command[1] == 'secondary':
-            send_request_to_slave('/createzoom', {'channel_id': channel_id})
+        else:
+            print(active_slaves[extracted_command[1]][1])
+            send_request_to_slave('/createzoom', {'channel_id': channel_id}, active_slaves[extracted_command[1]][0])
     elif extracted_command[0] == 'open':
         if extracted_command[1] == 'main':
             open_url_local(last_know_url)
-        elif extracted_command[1] == 'secondary':
-            send_request_to_slave('/openurl', {'url': last_know_url})
+        else:
+            print(active_slaves[extracted_command[1]][1])
+            send_request_to_slave('/openurl', {'url': last_know_url}, active_slaves[extracted_command[1]][0])
         last_know_url = ""
 
     delete_message(channel_id, ts)
@@ -202,43 +277,18 @@ def create_zoom():
     data = request.form
     channel_id = data.get('channel_id')
 
-    message_payload = {
-        "channel": channel_id,
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Select a meeting room where you want to create a meeting\n*Available Meeting Rooms:*"
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Main Meeting Room"
-                        },
-                        "action_id": "create:main"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Secondary Meeting Room"
-                        },
-                        "action_id": "create:secondary"
-                    }
-                ]
-            }
-        ]
-    }
+    ping_all_slaves()
+
+    message_payload = generate_blocks(
+        "Select a meeting room where you want to create a meeting\n*Available Meeting Rooms:*",
+        channel_id,
+        active_slaves,
+        "create",
+    )
 
     slack_client.chat_postMessage(**message_payload)
     return Response(), 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=42069)
+    app.run(debug=True, host='0.0.0.0', port=42069)
